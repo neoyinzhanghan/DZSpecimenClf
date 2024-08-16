6935b691fb9d27c2e2822cc5dfcaa5860cd54b89
Neo Yin <yinzhang@berkeley.edu>
2024-08-16 10:23:12 -0700

    lightning training script

--- extract_topviews.py
+++ extract_topviews.py
@@ -8,7 +8,7 @@ import ray
 ray.init()
 
 # Directory containing the .ndpi files
-input_dir = '/media/hdd3/neo/BMA_AML'
+input_dir = '/media/hdd3/neo/PB_slides'
 
 # Define an actor class to process .ndpi files
 @ray.remote
