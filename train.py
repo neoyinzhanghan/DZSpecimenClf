b17060177a59905cb8b70d2865cb052b8ddd3f81
Neo Yin <yinzhang@berkeley.edu>
2024-08-20 01:04:31 -0700

    model lighting training script

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ def validate(
 def main():
     metadata_file = "/home/greg/Documents/neo/wsi_specimen_clf_metadata.csv"
     batch_size = 1
-    N = 8  # Example value
+    N = 4  # Example value
     k = 9  # Example value
     num_classes = 2  # Number of classes in your dataset
     save_path = "best_model.pth"
