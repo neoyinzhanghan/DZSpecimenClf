0b7ec7526b335abc44ad4c928278d61497e09b38
Neo Yin <yinzhang@berkeley.edu>
2024-09-23 22:50:57 -0700

    concurrent processing for LLBMA

--- save_backward_grad.py
+++ save_backward_grad.py
@@ -42,13 +42,13 @@ def save_gradient_range_to_json(gradients, file_name="backward_gradients_range.j
 
 if __name__ == "__main__":
     from dataset import NDPI_DataModule
-    from DZSpecimenClf import DZSpecimenClf
+    from DZSpecimenClfToy import DZSpecimenClfToy
     import torch.nn as nn
 
     class SpecimenClassifier(nn.Module):
         def __init__(self, N, num_classes=2, patch_size=224):
             super(SpecimenClassifier, self).__init__()
-            self.model = DZSpecimenClf(
+            self.model = DZSpecimenClfToy(
                 N, num_classes=num_classes, patch_size=patch_size
             )
 
