9d0172f0ba2430d4a7db5ff8e2feacde1c194961
Neo Yin <yinzhang@berkeley.edu>
2024-09-23 22:37:06 -0700

    concurrent processing for LLBMA

--- save_backward_grad.py
+++ save_backward_grad.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     metadata_file = "/home/greg/Documents/neo/wsi_specimen_clf_metadata.csv"
     batch_size = 1
     N = 1  # Example value
-    patch_size = 4
+    patch_size = 224
     num_classes = 2  # Number of classes in your dataset
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
